1b2b531b4d96e1066b8e64521fdd8a16b2f14920
Arsh832 <128450141+Arsh832@users.noreply.github.com>
2024-06-13 10:37:04 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+
+<html>
+ <head>
+ <title>Data from HTML Page</title>
+ </head>
+ <body>
+ <?php
+echo "Name : " . $_POST["cname"]."</br>"; 
+echo "Email: " . $_POST["cemail"]."</br>"; 
+echo "Message: " . $_POST["cmessage"]."</br>"; 
+?>
+ </body>
+</html>
